2e09dd79547762cedd95416936ab0f0f2ff2a6de
Sébastien <78562421+cmoagcod@users.noreply.github.com>
2024-04-16 18:53:49 +0200

    Ajout des puissances (non fini)

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def debug_print(*args):
 
 class Fraction:
     def __init__(self, dividende: int, diviseur: int = 1):
-        debug_print("\nDébut du traitement de la fraction", dividende, "/", diviseur)
+        debug_print("Début du traitement de la fraction", dividende, "/", diviseur)
         if dividende < 0 and diviseur < 0:
             dividende = dividende * -1
             diviseur = diviseur * -1
-            debug_print("Fraction: La fraction est du type -", dividende, " / -", diviseur, ", elle a été transformée en",
-                  dividende, "/", diviseur)
+            debug_print("Fraction: La fraction est du type -", dividende, " / -", diviseur,
+                        ", elle a été transformée en", dividende, "/", diviseur)
 
         dividende_avant = None
         while dividende != dividende_avant:
@@ -25,12 +25,15 @@ class Fraction:
                     diviseur = int(diviseur / i)
                     dividende = int(dividende / i)
 
-        debug_print("La fraction finale est", dividende, "/", diviseur)
+        debug_print("La fraction finale est", dividende, "/", diviseur, "\n")
         self.dividende = dividende
         self.diviseur = diviseur
 
     def get(self) -> tuple:
         return self.dividende, "/", self.diviseur
+
+    def evaluate(self, precision: int):
+        return round(self.dividende / self.diviseur, precision)
 
 
 class Variable:
@@ -58,20 +61,41 @@ class Multiplication:
     def __init__(self, params: list):
         self.params = params
 
-    def get(self):
+    def get(self) -> tuple:
         resultat = []
         for i in self.params:
             resultat += i, "*"
         resultat.pop(len(resultat)-1)
+        resultat = tuple(resultat)
+        return resultat
+
+
+class Puissance:
+    def __init__(self, params: list):
+        self.params = params
+
+    def get(self) -> tuple:
+        resultat = []
+        for i in self.params:
+            resultat += i, "^"
+        resultat.pop(len(resultat)-1)
+        resultat = tuple(resultat)
         return resultat
 
 
 def ajouter(param: list[Fraction]) -> Fraction:
-    debug_print("\nDébut du traitement d'une addition")
+    debug_print("Début du traitement d'une addition")
+    resultat: Fraction = Fraction(0)
+    for i in param:
+        resultat = Fraction((resultat.dividende * i.diviseur) + (i.dividende * resultat.diviseur),
+                            resultat.diviseur * i.diviseur)
+        debug_print("Ajouter: Le résultat est désormais", resultat.get())
+    debug_print("Fin du traitement de l'addition\n")
+    return resultat
 
 
 def multiplier(param: list[Fraction]) -> Fraction:
-    debug_print("\nDébut du traitement d'une multiplication")
+    debug_print("Début du traitement d'une multiplication")
     resultat = Fraction(1)
     for i in param:
         if resultat.get() == Fraction(0).get() or i.get() == Fraction(0).get():
@@ -79,7 +103,23 @@ def multiplier(param: list[Fraction]) -> Fraction:
             return Fraction(0)
         resultat = Fraction(resultat.dividende * i.dividende, resultat.diviseur * i.diviseur)
         debug_print("Multiplier: Le résultat est désormais", resultat.get())
-    debug_print("Fin du traitement de la multiplication")
+    debug_print("Fin du traitement de la multiplication\n")
+    return resultat
+
+
+def exponent(param: list[Fraction] = [Fraction(0)]) -> Puissance:
+    debug_print("Début du traitement d'une puissance")
+    execute: bool = False
+    fraction_originale = Fraction(param[0].dividende, param[0].diviseur)
+    liste_exposants: list[Fraction] = []
+    for i in param:
+        if execute:
+            liste_exposants.append(i)
+        execute = True
+    local_exponent = multiplier(liste_exposants)
+    if local_exponent.diviseur == 1:
+        resultat = Fraction(fraction_originale.dividende ** local_exponent.dividende, fraction_originale.diviseur ** local_exponent.dividende)
+    debug_print("Fin du traitement de la puissance\n")
     return resultat
 
 
@@ -90,4 +130,5 @@ def conversion_fraction(nombre: float) -> Fraction:
 
 if __name__ == '__main__':
 
+    debug_print(exponent([Fraction(6, 2), Fraction(2)]).get())
     print('Programme terminé !')
