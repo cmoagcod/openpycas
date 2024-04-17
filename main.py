23f386799cfc422d4925620c4288499b81b1cac0
Sébastien <78562421+cmoagcod@users.noreply.github.com>
2024-04-17 11:33:38 +0200

    Ajout d'une liste de racines parfaites

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 nombres_premiers: list[int] = [2, 3, 5, 7, 11, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+racines: dict = {1: 1, 4: 2, 9: 3, 16: 4, 25: 5, 36: 6, 49: 7, 64: 8, 81: 9, 100: 10, 121: 11, 144: 12, 169: 13,
+                 196: 14, 225: 15, 256: 16, 289: 17, 324: 18, 361: 19, 400: 20, 441: 21, 484: 22, 529: 23, 576: 24,
+                 625: 25, 676: 26, 729: 27, 784: 28, 841: 29, 900: 30, 961: 31, 1024: 32, 1089: 33, 1156: 34, 1225: 35,
+                 1296: 36, 1369: 37, 1444: 38, 1521: 39, 1600: 40, 1681: 41, 1764: 42, 1849: 43, 1936: 44, 2025: 45,
+                 2116: 46, 2209: 47, 2304: 48, 2401: 49, 2500: 50, 2601: 51, 2704: 52, 2809: 53, 2916: 54, 3025: 55,
+                 3136: 56, 3249: 57, 3364: 58, 3481: 59, 3600: 60, 3721: 61, 3844: 62, 3969: 63, 4096: 64, 4225: 65,
+                 4356: 66, 4489: 67, 4624: 68, 4761: 69, 4900: 70, 5041: 71, 5184: 72, 5329: 73, 5476: 74, 5625: 75,
+                 5776: 76, 5929: 77, 6084: 78, 6241: 79, 6400: 80, 6561: 81, 6724: 82, 6889: 83, 7056: 84, 7225: 85,
+                 7396: 86, 7569: 87, 7744: 88, 7921: 89, 8100: 90, 8281: 91, 8464: 92, 8649: 93, 8836: 94, 9025: 95,
+                 9216: 96, 9409: 97, 9604: 98, 9801: 99, 10000: 100}
 debug: bool = True
 
 
@@ -118,7 +128,8 @@ def exponent(params: list[Fraction]) -> object:
         execute = True
     local_exponent = multiplier(liste_exposants)
     if local_exponent.diviseur == 1:
-        resultat = Fraction(fraction_originale.dividende ** local_exponent.dividende, fraction_originale.diviseur ** local_exponent.dividende)
+        resultat = Fraction(fraction_originale.dividende ** local_exponent.dividende,
+                            fraction_originale.diviseur ** local_exponent.dividende)
     else:
         racine = Fraction(1, local_exponent.diviseur)
         puissance = Fraction(local_exponent.dividende)
@@ -141,10 +152,4 @@ def conversion_fraction(nombre: float) -> Fraction:
 
 if __name__ == '__main__':
 
-    test_exponent = exponent([Fraction(24, 20), Fraction(2)])
-    if isinstance(test_exponent, Fraction):
-        debug_print(test_exponent.get())
-        debug_print(test_exponent.evaluate(5))
-    else:
-        debug_print(test_exponent.params[0].get(), test_exponent.params[1].get())
     print('Programme terminé !')
